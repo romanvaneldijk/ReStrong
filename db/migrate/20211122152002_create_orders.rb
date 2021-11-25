0d98cebddf97c7e392f154b5dd8f443bc6827bb9
Roman van Eldijk <romanvaneldijk@gmail.com>
2021-11-25 11:01:24 +0100

    deleted item_id from the order table

--- db/migrate/20211122152002_create_orders.rb
+++ db/migrate/20211122152002_create_orders.rb
@@ -1,7 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.references :item, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
