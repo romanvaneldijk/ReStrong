ff407dc26f2c5d726d80a56542b4ac7c579c277d
eric <draheim.eric@googlemail.com>
2021-11-25 17:53:09 +0100

    orders

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,5 +1,6 @@
 class OwnersController < ApplicationController
   def show
     @orders = Order.all
+    @dinners = Dinner.all
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,7 @@
 class Item < ApplicationRecord
-<<<<<<< HEAD
   has_many :order_items
   validates :name, presence: true
   validates :price, presence: true
   validates :description, presence: true
-=======
   has_many :order_items, dependent: :destroy
->>>>>>> master
 end
