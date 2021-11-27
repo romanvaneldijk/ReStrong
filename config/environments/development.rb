038d3d996880c9c2887a28f65c20d3ba44bafa59
Javid Allahverdiyev <javid.allahverdiyev@hotmail.com>
2021-11-27 20:31:10 +0100

    changed the icon on the browser tab

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-  config.hosts << "775c-213-61-167-82.ngrok.io"
+  config.hosts << "235f-213-61-167-82.ngrok.io"
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
