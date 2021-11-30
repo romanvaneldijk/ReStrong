82cacd73192ed41f51956695114a3e048d565f91
Roman van Eldijk <romanvaneldijk@gmail.com>
2021-11-30 10:06:09 +0100

    finished backend for current orders page

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -15,23 +15,6 @@ class OrderItemsController < ApplicationController
     end
   end
 
-  # def create_men
-  #   @user = current_user
-  #   @item = Item.find(params[:item_id])
-  #   # @item = Item.find_by(status: "processing").present?
-  #   if @user.orders.find_by(status: "processing").present?
-  #     @order = @user.orders.find_by(status: "processing")
-  #   else
-  #     @order = Order.create(user: @user)
-  #   end
-  #   OrderItem.create(order: @order, item: @item)
-
-
-  #     redirect_to request.referer
-  #   end
-  # end
-
-
   def destroy
     @user = current_user
     @item = Item.find(params[:item_id])
