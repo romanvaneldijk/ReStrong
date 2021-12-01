edf9403b80990b2487f68625af000f6a44465860
eric <draheim.eric@googlemail.com>
2021-12-01 15:57:33 +0100

    seeded

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -4,13 +4,6 @@ class OwnersController < ApplicationController
     @dinners = Dinner.all
     @tables = Table.all
   end
-
-  def orders
-    @orders = Order.all
-    @dinners = Dinner.all
-    @tables = Table.all
-  end
-
   def menu
     @items = Item.all
     @item = Item.new
